eea1e52c13253ece8dfdd68f730902629600bbff
YousukeTezuka <YousukeTezuka@gmail.com>
2015-09-04 18:00:10 +0900

    Build a Basic UI

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by 手塚 陽介 on 2015/09/04.
+//  Copyright © 2015年 YousukeTezuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
